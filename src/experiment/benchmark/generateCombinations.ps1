4d550442ce670c6d62d612befc93390c530292ea
Olivier Brochu <olivier.brochu.1@ens.etsmtl.ca>
2025-08-20 15:12:09 -0400

    Rewrote the script for the bitrate, visual quality and encoding time benchmark. FFmpeg commands are to be replaced with their NVENC version.

---
+++ src/experiment/benchmark/generateCombinations.ps1
@@ -0,0 +1,38 @@
+param(
+	# An array of tiles to encode
+	[Parameter(Mandatory=$true)][String[]] $tiles,
+	# An array of codecs. Codecs must be supported by by FFmpeg
+	[Parameter(Mandatory=$true)][String[]] $codecs,
+	# An array of presets
+	[Parameter(Mandatory=$true)][String[]] $presets,
+	# An array of constant quantization parameters between 0 and 51
+	[Parameter(Mandatory=$true)][int[]] $qps,
+	# An array of heights. The aspect ratio is kept. An height of 0 means no resizing
+	[Parameter(Mandatory=$true)][int[]] $heights,
+	# The csv file where the data will be saved to
+	[Parameter(Mandatory=$true)][String] $outputFile
+)
+
+$output = [System.Text.StringBuilder]::new()
+
+# Write the header of the csv file
+[void]$output.AppendLine("tile,segment,codec,preset,qp,height,bitrate,meanVmaf,meanPsnrY,meanPsnrCb,meanPsnrCr,logFile,time")
+
+foreach ($tile in $tiles)
+{
+	foreach ($codec in $codecs)
+	{
+		foreach ($preset in $presets)
+		{
+			foreach ($qp in $qps)
+			{
+				foreach ($height in $heights)
+				{
+                    [void]$output.AppendLine("$tile,,$codec,$preset,$qp,$height,,,,,,,")
+                }
+            }
+        }
+    }
+}
+
+$output.ToString() | Out-File -FilePath $outputFile -Encoding utf8
